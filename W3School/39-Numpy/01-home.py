1af48412df24bada5a269ab1142415bdced76dd9
emidaniele98 <emidaniele98@gmail.com>
2024-06-14 08:59:28 -0300

    w3school

---
+++ W3School/39-Numpy/01-home.py
@@ -0,0 +1,12 @@
+### NumPy Tutorial ###
+
+''' 
+NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays.
+'''
+
+# Learning by Examples
+import numpy as np
+# Create a numpy array
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+print(type(arr))
